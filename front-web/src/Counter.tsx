d952d08183ee2a78e929ff35a929421fa6b95442
mauriciolariao <mauricio.lariao@gmail.com>
2021-01-10 14:57:15 -0300

    Create front-web initial files

---
+++ front-web/src/Counter.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function Counter (){
+
+  //conceito de estado
+  const [counter, setCounter] = useState(0);
+
+  const handleIncrease = () => {
+    setCounter(counter +1);
+  }
+
+  const handleDecrease = () => {
+    if (counter >= 1) {
+      setCounter(counter -1)
+    }
+   
+  }
+
+  return(
+    <div>
+      <button onClick={handleIncrease}>Incrementar</button>
+      <button onClick={handleDecrease}>Decrementar</button>
+  <h1>Valor do contador: {counter}</h1>
+    </div>
+  )
+}
+
+export default Counter;
